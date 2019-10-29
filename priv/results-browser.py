5cfe5d12c65791efa4823737246fbcf9d1f4e7ce
Randy Secrist <rsecrist@sofi.org>
2019-10-29 09:18:25 -0600

    update results browser to work with python 3 (janky af)

--- priv/results-browser.py
+++ priv/results-browser.py
@@ -1,33 +1,39 @@
  #!/usr/bin/env python
 
-import SimpleHTTPServer
-import SocketServer
+import http.server
+import socketserver
 import logging
 import cgi
 import base64
 import argparse
 import os
 
-class ServerHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
+class ServerHandler(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
       logging.warning("======= GET STARTED =======")
       logging.warning(self.headers)
-      SimpleHTTPServer.SimpleHTTPRequestHandler.do_GET(self)
+      http.server.SimpleHTTPRequestHandler.do_GET(self)
 
     def do_POST(self):
       logging.warning("======= POST STARTED =======")
       length = self.headers['content-length']
       data = self.rfile.read(int(length))
 
-      with open(os.path.join("." , "summary.png"), 'w') as fh:
+      # I don't know wtf is wrong with this handler, it is called twice
+      # on a page load and only saves one blank canvas and one of the 5 graphs
+      # (using content length to differentiate the requests for now)
+      with open(os.path.join("." , "summary_raw_py_" + length + ".txt"), 'wb') as fh:
+        fh.write(data)
+
+      with open(os.path.join("." , "summary_py_" + length + ".png"), 'wb') as fh:
         fh.write(base64.b64decode(data.decode()))
 
       self.send_response(200)
 
 
 def startServer(host, port):
-  httpd = SocketServer.TCPServer((host, port), ServerHandler)
-  print 'Serving at: http://{host}:{port}'.format(host=host, port=port)
+  httpd = socketserver.TCPServer((host, port), ServerHandler)
+  print('Serving at: http://{host}:{port}'.format(host=host, port=port))
   httpd.serve_forever()
 
 if __name__ == '__main__':
